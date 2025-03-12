6740e4eaf88c7e1408fc8fc79f74406a1a33254d
MoeurnSovanara <liip28369@gmail.com>
2025-03-12 12:47:41 +0700

    First Commit Spring basic REST API

---
+++ src/main/java/org/example/springbasicrestapi/SpringBasicRestapiApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbasicrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbasicrestapi/SpringBasicRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbasicrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
